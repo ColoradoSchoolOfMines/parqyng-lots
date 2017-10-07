6a5a1e4da0f6b2b9fe8f9c73cea8ce59983ba334
daichij <djameson@mines.edu>
2017-10-07 09:33:05 +0000

    Started working on identifying individuals walking through frame

--- demo/cvTest.py
+++ demo/cvTest.py
@@ -29,12 +29,19 @@ print("Capture device is open?: " + str(cap.isOpened()))
 #eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 try:
     first_frame = None
+    frame_type = "colo"
     run = True
     while run:
         ret, frame = cap.read()
         check_exit = stdscr.getch()
         if check_exit == ord('q'):
             run = False
+        '''
+        elif check_exit == ord('c'):
+            frame_type = 'colo'
+        elif check_exit == ord('g'):
+            frame_type = 'gray'
+        '''
         
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -44,16 +51,30 @@ try:
 
         frame_delta = cv2.absdiff(first_frame, gray)
         ret, thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=2)
         gray = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
         '''
+        # Uploading commented out, broken code. #Itsahackathonthatsmyexcuse
+        (cnts, _) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 
+            cv2.CHAIN_APPROX_SIMPLE)
+        for c in cnts:
+            if cv2.contourArea(c) < 10:
+                continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         '''
         if ret:
             outframe = hdmi_out.newframe()
-            #outframe[:] = frame
-            outframe[:] = gray
+            #if frame_type == 'colo':
+            outframe[:] = frame
+            '''
+            elif frame_type == 'gray':
+                outframe[:] = gray
+            '''
             hdmi_out.writeframe(outframe)
         else:
             raise RuntimeError("Error while reading from camera")
